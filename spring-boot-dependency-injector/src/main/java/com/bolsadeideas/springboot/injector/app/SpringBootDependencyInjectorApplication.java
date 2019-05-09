335e3dbda1fb19833a6cef5386c621941bbb5adf
Pablo Pinto <pabloctender@gmail.com>
2019-05-09 22:50:08 +0200

    First Push

---
+++ spring-boot-dependency-injector/src/main/java/com/bolsadeideas/springboot/injector/app/SpringBootDependencyInjectorApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.injector.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDependencyInjectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDependencyInjectorApplication.class, args);
+	}
+
+}
