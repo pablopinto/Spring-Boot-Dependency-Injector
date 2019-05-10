28116728766a8063064590f43f4612f168ae9535
Pablo Pinto <pabloctender@gmail.com>
2019-05-10 13:29:57 +0200

    @Autowired

--- spring-boot-dependency-injector/src/main/java/com/bolsadeideas/springboot/injector/app/controllers/IndexController.java
+++ spring-boot-dependency-injector/src/main/java/com/bolsadeideas/springboot/injector/app/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.bolsadeideas.springboot.injector.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- spring-boot-dependency-injector/src/main/java/com/bolsadeideas/springboot/injector/app/controllers/models/service/MiServicioComplejo.java
+++ spring-boot-dependency-injector/src/main/java/com/bolsadeideas/springboot/injector/app/controllers/models/service/MiServicioComplejo.java
@@ -3,13 +3,13 @@ package com.bolsadeideas.springboot.injector.app.controllers.models.service;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Primary
-@Component("miServicioSimple")
-public class MiServicio implements IServicio {
+
+@Component("miServicioComplejo")
+public class MiServicioComplejo implements IServicio {
 
 	@Override
 	public String operacion() {
-		return "ejecutando algun proceso importante simple ";
+		return "ejecutando algun proceso importante complejo";
 	}
 
 }
